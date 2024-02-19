a9feb5cb595705bbe6ad6692092beba85967350a
Falih <falihdzakwan028@gmail.com>
2024-02-19 08:11:51 +0700

    feat: session provider and disable navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,30 @@
+"use client"
+
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SessionProvider } from 'next-auth/react'
 import { Gabarito, Roboto } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import './globals.css'
+import { useRouter } from "next/navigation"
 
 const gabarito = Gabarito({ subsets: ['latin'] })
-const roboto = Roboto({ 
+const roboto = Roboto({
   weight: ['400', '700'],
-  subsets: ['latin'] 
+  subsets: ['latin']
 })
 
-export const metadata = {
-  title: 'YourAnimeList',
-  description: 'Your Beloved Indonesian Anime Website',
-}
+const disableNavbar = ["/login", "register"]
 
 export default function RootLayout({ children }) {
+  const router = useRouter()
+  const pathName = router.pathname
   return (
     <html lang="en">
       <body className={`${gabarito.className || roboto.className} bg-color-dark`} suppressHydrationWarning={true}>
-        <Navbar />
-        {children}
+        <SessionProvider>
+          {disableNavbar.includes(pathName) && <Navbar />}
+          {children}
+        </SessionProvider>
       </body>
     </html>
   )
