7b16d6706fec7d6a20002261f45b6bb5ec9809d4
Falih <falihdzakwan028@gmail.com>
2024-02-23 22:09:33 +0700

    feat: handling user login

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,62 @@
+import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth/next"
+import { login } from "@/libs/auth-libs";
+import { compare } from "bcrypt";
+
+const authOptions  = {
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            type: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email"},
+                password: { label: "Password", type: "password"},
+            },
+
+            async authorize (credentials) {
+                const { email, password } = credentials
+                const user = await login({ email, password }) 
+
+                if(user) {
+                    const passwordConfirm = await compare(password, user.password)
+                    if(passwordConfirm) {
+                        return user
+                    }
+                    return null
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, account, profile, user }) {
+            if (account?.provider === "credentials"){
+                token.email = user.email;
+                token.name = user.name;
+            }
+            return token;
+        },
+
+        async session({session, token}){
+            if ("email" in token) {
+                session.user.email = token.email;
+            }
+            if ("name" in token) {
+                session.user.name = token.name;
+            }
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login"
+    }
+};
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
