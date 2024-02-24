d61c53dc4fbe48d573ea84e44c4a3337aad8ece2
Falih <falihdzakwan028@gmail.com>
2024-02-24 15:50:48 +0700

    feat: implement requireAuth from middleware.js, implement token redirect

--- src/middlewares/withAuth.js
+++ src/middlewares/withAuth.js
@@ -1,19 +1,29 @@
 import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
 
-export default function withAuth(middleware){
+const authPage = ["/login", "/register"]
+
+export default function withAuth(middleware, requireAuth){
     return async (req, next) => {
         const pathname = req.nextUrl.pathname
 
-        if(pathname.startsWith("/users")){
+        if(pathname.startsWith("/users") || requireAuth.includes(pathname)){
             const token = await getToken({
                 req,
                 secret: process.env.NEXTAUTH_SECRET
             })
-            if(!token) {
+            if(!token && !authPage.includes(pathname)) {
                 const url = new URL("/login", req.url)
                 url.searchParams.set("callbackUrl", encodeURI(req.url))
-                return Response.redirect(url)
+                return NextResponse.redirect(url)
             }
+
+            if(token) {
+                if(authPage.includes(pathname)){
+                    return NextResponse.redirect(new URL("/", req.url))
+                }
+            }
+
             return middleware(req, next)
         }
     }
