9db4e14ae737f9cbda5393e48f2f72d2b256330c
Falih <falihdzakwan028@gmail.com>
2024-02-25 21:50:36 +0700

    fix: rating query fix

--- src/app/users/dashboard/rating/page.jsx
+++ src/app/users/dashboard/rating/page.jsx
@@ -1,29 +1,30 @@
-import Header from "@/components/Dashboard/DashboardHeader"
+import Header from "@/components/Dashboard/Header"
 import { authUserSession } from "@/libs/auth-libs"
 import prisma from "@/libs/prisma"
 import Link from "next/link"
+import RatingDisplay from "@/components/Dashboard/RatingDisplay"
 
 const page = async () => {
     const user = await authUserSession()
-    const comments = await prisma.comment.findMany({where: {user_email: user.email}})
+    const ratings = await prisma.rating.findMany({where: {user_email: user.email}})
 
     return (
         <section className="mt-4 px-4 w-full">
             <Header title="My Ratings" />
-            {comments.length > 0 ? 
+            {ratings.length > 0 ? 
             <div className="grid grid-cols-1 gap-4 py-2">
-                {comments.map(comment => {
+                {ratings.map(rating => {
                     return (
-                        <Link href={`/anime/${comment.anime_mal_id}`} key={comment.id} className="text-color-dark bg-color-primary p-4 rounded">
-                            <p className="font-bold text-md">{comment.anime_title}</p>
-                            <p className="italic">{comment.comment}</p>
+                        <Link href={`/anime/${rating.anime_mal_id}`} key={rating.id} className="text-color-dark bg-color-primary p-4 rounded">
+                            <p className="font-bold text-md">{rating.anime_title}</p>
+                            <RatingDisplay rating={rating.rating} />
                         </Link>
                     )
                 })}
             </div>
             :
             <div className="flex justify-center items-center h-64">
-                    <p className="text-color-primary text-2xl">Belum ada komentar...</p>
+                    <p className="text-color-primary text-2xl">Belum ada rating...</p>
             </div>
             }
         </section>
