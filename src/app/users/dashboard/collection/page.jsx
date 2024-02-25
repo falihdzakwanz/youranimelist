a87e5b98bc1c32fd74e5e30d345d3734e236c036
Falih <falihdzakwan028@gmail.com>
2024-02-25 21:49:49 +0700

    style: add belum ada collection text if collection = 0

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -7,26 +7,31 @@ import Link from "next/link"
 const Page = async () => {
     const user = await authUserSession()
     const collection = await prisma.collection.findMany({
-        where: {user_email: user.email}
+        where: { user_email: user.email }
     })
-    // console.log("🚀 ~ Page ~ collection:", collection)
 
-    return(
+    return (
         <section className="mt-4 px-4 w-full">
-            <Header title="My Collections"/>
-            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                {collection.map((collect, index) => {
-                    return(
-                        <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative border-2 border-color-accent rounded overflow-hidden">
-                            <Image src={collect.anime_image_webp} alt={collect.anime_image_jpg} width={350} height={350} className="w-full"/>
-                            <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-                                <h5 className="text-xl text-center">{collect.anime_title}</h5>
-                            </div>
-                        </Link>
-                    )
-                })}
+            <Header title="My Collections" />
+            {collection.length > 0 ?
+                <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                    {collection.map((collect, index) => {
+                        return (
+                            <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative border-2 border-color-accent rounded overflow-hidden">
+                                <Image src={collect.anime_image_webp} alt={collect.anime_image_jpg} width={350} height={350} className="w-full" />
+                                <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+                                    <h5 className="text-xl text-center">{collect.anime_title}</h5>
+                                </div>
+                            </Link>
+                        )
+                    })}
 
-            </div>
+                </div>
+                :
+                <div className="flex justify-center items-center h-64">
+                    <p className="text-color-primary text-2xl">Belum ada collection...</p>
+                </div>
+            }
         </section>
     )
 }
