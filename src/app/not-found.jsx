5f473458b01ca31b114c1f4d52cacc65d6b1bdef
Falih <falihdzakwan028@gmail.com>
2024-02-25 21:20:25 +0700

    style: add 404 text

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -11,7 +11,7 @@ const Page = () => {
             <div className="flex justify-center items-center flex-col gap-5">
                 <div className="flex gap-3 items-center justify-center">
                     <FileSearch size={44} className="text-color-accent" />
-                    <h2 className="text-color-accent text-4xl font-bold">NOT FOUND</h2>
+                    <h2 className="text-color-accent text-4xl font-bold">404 | NOT FOUND</h2>
                 </div>
                 <button onClick={() => router.back()} className="bg-color-primary hover:text-color-accent transition-all p-3 rounded">Kembali</button>
             </div>
