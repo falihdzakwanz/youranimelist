53579c13335d67b4db303f5f2a99536cb514654a
Falih <falihdzakwan028@gmail.com>
2024-02-25 15:39:03 +0700

    feat: implement add anime to collection if user true, implement add rating if user true, implement add comment if user true

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -5,14 +5,25 @@ import CommentInput from "@/components/AnimeList/CommentInput"
 import CommentBox from "@/components/AnimeList/CommentBox"
 import RatingInput from "@/components/AnimeList/RatingInput"
 import RatingAverage from "@/components/AnimeList/RatingAverage";
+import { authUserSession } from "@/libs/auth-libs"
+import CollectionButton from "@/components/AnimeList/CollectionButton"
+import Link from "next/link"
 
 const Page = async ({params: {id}}) => {
     const anime = await getAnimeResponse(`anime/${id}`)
+    const user = await authUserSession()
+    const collection = await prisma.collection.findFirst({
+        where: { user_email: user?.email, anime_mal_id: id }
+    })
+    const rating = await prisma.rating.findFirst({
+        where: { user_email: user?.email, anime_mal_id: id }
+    })
 
     return (
         <>
         <div className="pt-4 px-4">
             <h2 className="lg:text-2xl text-xl text-color-primary">{anime.data.title} - {anime.data.year}</h2>
+            {user && <CollectionButton id={collection?.id} anime_mal_id={id} anime_title={anime.data.title} anime_image_webp={anime.data.images.webp.image_url}  anime_image_jpg={anime.data.images.jpg.image_url} dbStatus={collection ? true : false}/>}
         </div>
 
         <div className="pt-4 px-4 flex flex-wrap gap-2 text-color-primary">
@@ -47,16 +58,17 @@ const Page = async ({params: {id}}) => {
         </div>
         <div className="pt-4 px-4 text-color-primary">
             <p className="text-justify sm:text-sm lg:text-xl">{anime.data.synopsis}</p>
-            <a href={anime.data.url} target="_blank" rel="noopener noreferrer" className="underline text-sm lg:text-lg text-color-accent">Official Website</a>
+            <a href={anime.data.url} target="_blank" rel="noopener noreferrer" className="underline text-sm lg:text-base text-color-accent">Official Website</a>
         </div>
         <div className="p-4">
-            <RatingInput anime_mal_id={id} anime_title={anime.data.title} />
-            <p className="text-color-primary text-md lg:text-xl mt-2">Rata-Rata: <RatingAverage anime_mal_id={id} />/5.0</p>
+            {user && <RatingInput anime_mal_id={id} anime_title={anime.data.title} rating={rating?.rating} id={rating?.id}/>}
+            <p className="text-color-primary text-base lg:text-md mt-2">Average: <RatingAverage anime_mal_id={id} />/5.0</p>
         </div>
         <div className="p-4">
-            <h3 className="text-color-primary text-md lg:text-2xl mb-2">Komentar</h3>
+            <h3 className="text-color-primary text-md lg:text-xl mb-2">Comments</h3>
             <CommentBox anime_mal_id={id} />
-            <CommentInput anime_mal_id={id} anime_title={anime.data.title}/>
+            {user && <CommentInput anime_mal_id={id} anime_title={anime.data.title}/>}
+            {!user && <Link href="/login" className="text-base text-color-accent text-center underline">Log In to post a comment</Link>}
         </div>
         </>
     )
