b8f55254c8894027dcf21817225f0175796207e2
Falih <falihdzakwan028@gmail.com>
2024-02-25 21:20:54 +0700

    feat: implement error page

---
+++ src/app/error.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+const Page = () => {
+
+    return (
+        <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
+            <div className="flex justify-center items-center flex-col gap-5">
+                <div className="flex gap-3 items-center justify-center">
+                    <h2 className="text-color-accent text-4xl font-bold">500 | SOMETHING WENT WRONG</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page
